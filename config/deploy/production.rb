50fb0d98bcbd386c4ff6f5509691a61dd4c08121
Kevin Jung <admin@kjung.ca>
2014-07-24 19:22:53 -0400

    Updated deploy path

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 set :stage, :production
 set :branch, ENV['BRANCH'] || :develop
-set :application, 'kevinjung.ca/public/'
+set :application, 'kevinjung.ca/'
 set :deploy_to, "/home/kjung/websites/#{fetch(:application)}"
 
 # server '192.210.137.216', user: 'root', roles: %w{web app db}
